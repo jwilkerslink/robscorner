50d7c95ab53df90ae2a57221bda661bd210fe054
Roberto Veloz <rveloz@bi.com>
2020-05-20 15:30:44 -0600

    Commit

---
+++ RFID/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace RFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.bwNotifications = new System.ComponentModel.BackgroundWorker();
+            this.bwConnect = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tbNotify = new System.Windows.Forms.TabPage();
+            this.tbTrack = new System.Windows.Forms.TabPage();
+            this.dgvTracker = new System.Windows.Forms.DataGridView();
+            this.tbUnitHistory = new System.Windows.Forms.TabPage();
+            this.TagID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Location = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Event = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tbNotify.SuspendLayout();
+            this.tbTrack.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTracker)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(762, 350);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TagList";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(470, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Notifications";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // bwNotifications
+            // 
+            this.bwNotifications.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwNotifications_DoWork);
+            this.bwNotifications.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwNotifications_RunWorkerCompleted);
+            // 
+            // bwConnect
+            // 
+            this.bwConnect.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwConnect_DoWork);
+            this.bwConnect.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwConnect_RunWorkerCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tbNotify);
+            this.tabControl1.Controls.Add(this.tbTrack);
+            this.tabControl1.Controls.Add(this.tbUnitHistory);
+            this.tabControl1.Location = new System.Drawing.Point(12, 49);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 389);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tbNotify
+            // 
+            this.tbNotify.Controls.Add(this.textBox1);
+            this.tbNotify.Location = new System.Drawing.Point(4, 29);
+            this.tbNotify.Name = "tbNotify";
+            this.tbNotify.Padding = new System.Windows.Forms.Padding(3);
+            this.tbNotify.Size = new System.Drawing.Size(768, 356);
+            this.tbNotify.TabIndex = 0;
+            this.tbNotify.Text = "Notifications";
+            this.tbNotify.UseVisualStyleBackColor = true;
+            // 
+            // tbTrack
+            // 
+            this.tbTrack.Controls.Add(this.dgvTracker);
+            this.tbTrack.Location = new System.Drawing.Point(4, 29);
+            this.tbTrack.Name = "tbTrack";
+            this.tbTrack.Padding = new System.Windows.Forms.Padding(3);
+            this.tbTrack.Size = new System.Drawing.Size(768, 356);
+            this.tbTrack.TabIndex = 1;
+            this.tbTrack.Text = "Tracker";
+            this.tbTrack.UseVisualStyleBackColor = true;
+            // 
+            // dgvTracker
+            // 
+            this.dgvTracker.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvTracker.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTracker.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TagID,
+            this.Location,
+            this.Event,
+            this.Time});
+            this.dgvTracker.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTracker.Location = new System.Drawing.Point(3, 3);
+            this.dgvTracker.Name = "dgvTracker";
+            this.dgvTracker.RowHeadersWidth = 62;
+            this.dgvTracker.RowTemplate.Height = 28;
+            this.dgvTracker.Size = new System.Drawing.Size(762, 350);
+            this.dgvTracker.TabIndex = 0;
+            // 
+            // tbUnitHistory
+            // 
+            this.tbUnitHistory.Location = new System.Drawing.Point(4, 29);
+            this.tbUnitHistory.Name = "tbUnitHistory";
+            this.tbUnitHistory.Size = new System.Drawing.Size(768, 356);
+            this.tbUnitHistory.TabIndex = 2;
+            this.tbUnitHistory.Text = "UnitHistory";
+            this.tbUnitHistory.UseVisualStyleBackColor = true;
+            // 
+            // TagID
+            // 
+            this.TagID.HeaderText = "TagID";
+            this.TagID.MinimumWidth = 8;
+            this.TagID.Name = "TagID";
+            // 
+            // Location
+            // 
+            this.Location.HeaderText = "Location";
+            this.Location.MinimumWidth = 8;
+            this.Location.Name = "Location";
+            // 
+            // Event
+            // 
+            this.Event.HeaderText = "Event";
+            this.Event.MinimumWidth = 8;
+            this.Event.Name = "Event";
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Time";
+            this.Time.MinimumWidth = 8;
+            this.Time.Name = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tbNotify.ResumeLayout(false);
+            this.tbNotify.PerformLayout();
+            this.tbTrack.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTracker)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.ComponentModel.BackgroundWorker bwNotifications;
+        private System.ComponentModel.BackgroundWorker bwConnect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tbNotify;
+        private System.Windows.Forms.TabPage tbTrack;
+        private System.Windows.Forms.DataGridView dgvTracker;
+        private System.Windows.Forms.TabPage tbUnitHistory;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Location;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Event;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Time;
+    }
+}
+
---
+++ RFID/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using nsAlienRFID2;
+namespace RFID
+{
+    public partial class Form1 : Form
+    {
+
+        clsReaderMonitor Monitor = new clsReaderMonitor();
+        clsReader mReader = new clsReader();
+        const string reasonStr = "Reason:";
+
+        public delegate void AddRowDelegate(System.Windows.Forms.DataGridView ctrl, string tagID, string evnt, DateTime dateTime);
+        public static void AddRow(System.Windows.Forms.DataGridView ctrl, string tagID, string evnt, DateTime dateTime)
+        {
+            if (ctrl.InvokeRequired)
+            {
+
+                object[] params_list = new object[] { ctrl, tagID, evnt, dateTime };
+
+                ctrl.Invoke(new AddRowDelegate(AddRow), params_list);
+
+            }
+
+            else
+            {
+                char[] p = { ',' };
+                ctrl.Rows.Insert(0, tagID, evnt, dateTime);
+                //ctrl.Rows.Add();
+            }
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Monitor_ReaderRemoved(IReaderInfo data)
+        {
+            textBox1.Text = textBox1.Text + "\r\n Reader " + data.Name + " has been Removed. IP: " + data.IPAddress;
+
+
+            //throw new NotImplementedException();
+        }
+
+        private void Monitor_ReaderAdded(IReaderInfo data)
+        {
+
+            textBox1.Text = textBox1.Text + "\r\n Reader " + data.Name + " has been Added. IP: " + data.IPAddress;
+
+            //throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Monitor.ReaderAdded += Monitor_ReaderAdded;
+            //Monitor.ReaderRemoved += Monitor_ReaderRemoved;
+            //Monitor.ReaderAddedOnSerial += Monitor_ReaderAddedOnSerial;
+            //Monitor.ReaderRemovedOnSerial += Monitor_ReaderRemovedOnSerial;
+            ////mReader.MessageReceived += MReader_MessageReceived;
+            //Monitor.ComPortsMonitoring = true;
+            //mReader.NotifyMode = "ON";
+            //mReader.AutoMode = "ON";
+            //Monitor.CheckComPorts();
+            bwConnect.RunWorkerAsync();
+        }
+
+        //private void MReader_MessageReceived(string data)
+        //{
+        //    string tl;
+        //    ITagInfo[] tagInfos;
+        //    int start = data.IndexOf("#StopTriggerLines:");
+        //    if (start > 0)
+        //    {
+
+        //        start = data.IndexOf("Tag:", start);
+        //        int ennd = data.IndexOf("\r\n#End of Notification Message", start);
+
+        //        tl = data.Substring(start, ennd - start);
+
+        //        if (mReader.ParseTagList(tl, out tagInfos))
+        //        {
+        //            foreach (ITagInfo tag in tagInfos)
+        //            {
+        //                textBox1.Text = textBox1.Text + "\r\n TagID:" + tag.TagID + "\r\n Read Count:" + tag.ReadCount + "\r\n Last Seen:" + tag.LastSeenTime;
+        //                textBox1.Select(textBox1.Text.Length, 0);
+        //                textBox1.ScrollToCaret();
+        //            }
+        //        }
+
+        //    }
+
+        //    SetText(textBox1, data);
+
+
+        //    //ITagInfo[] tagInfos;
+        //    //if (mReader.ParseTagList(data, out tagInfos))
+        //    //{
+        //    //    foreach (ITagInfo tag in tagInfos)
+        //    //    {
+        //    //        SetText(textBox1, data);
+        //    //    }
+        //    //}
+
+            
+        //    //textBox1.Text = textBox1.Text + "\r\n" + data;
+        //    //textBox1.Select(textBox1.Text.Length, 0);
+        //    //textBox1.ScrollToCaret();
+        //}
+
+        public delegate void SetTextDelegate(System.Windows.Forms.TextBox ctrl, string text);
+
+        public static void SetText(System.Windows.Forms.TextBox ctrl, string text)
+        {
+
+            if (ctrl.InvokeRequired)
+            {
+
+                object[] params_list = new object[] { ctrl, text };
+
+                ctrl.Invoke(new SetTextDelegate(SetText), params_list);
+
+            }
+
+            else
+            {
+                ctrl.Text = ctrl.Text + text + "\r\n";
+                ctrl.Select(ctrl.Text.Length, 0);
+                ctrl.ScrollToCaret();
+                //if (text == "Pass")
+                //{
+                //    ctrl.BackColor = Color.LightGreen;
+                //}
+                //else if (text == "???")
+                //    ctrl.BackColor = Color.LightGray;
+                //else if (text == "Fail")
+                //    ctrl.BackColor = Color.Red;
+                // ctrl.SelectionStart = ctrl.Text.Length;
+                //  ctrl.ScrollToCaret();
+
+            }
+
+        }
+
+
+        private void Monitor_ReaderRemovedOnSerial(IReaderInfo data)
+        {
+            textBox1.Text = textBox1.Text + "\r\n Reader " + data.Name + " has been Removed. IP: " + data.IPAddress;
+
+            //throw new NotImplementedException();
+        }
+
+        private void Monitor_ReaderAddedOnSerial(IReaderInfo data)
+        {
+            textBox1.Text = textBox1.Text + "\r\n Reader " + data.Name + " has been Added. IP: " + data.IPAddress;
+
+            //throw new NotImplementedException();
+        }
+
+        private void Monitor_ReaderAddedSerial()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //String stemp;
+            //mReader.InitOnCom(3);// ‘Initialize reader object on COM1
+            //stemp = mReader.Connect();
+            //textBox1.Text = textBox1.Text + "\r\n" + stemp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ITagInfo[] tagInfos;
+            var tl = mReader.TagList;
+            //textBox1.Text = textBox1.Text + tl;
+            //textBox1.Select(textBox1.Text.Length, 0);
+            //textBox1.ScrollToCaret();
+            
+
+            if (mReader.ParseTagList(tl, out tagInfos))
+            {
+                foreach (ITagInfo tag in tagInfos)
+                {
+                    textBox1.Text = textBox1.Text + "TagID:" + tag.TagID + "\r\n Read Count:" + tag.ReadCount + "\r\n Last Seen:" + tag.LastSeenTime + "\r\n";
+                    textBox1.Select(textBox1.Text.Length, 0);
+                    textBox1.ScrollToCaret();
+                }
+            }
+                
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] temp;
+            int messages;
+
+            //mReader.NotifyMode = "ON";
+            //mReader.AutoMode = "ON";
+
+            //messages = mReader.GetCurrentMessages(out temp);
+            
+            //if (messages > 0)
+            //{
+            //    foreach(string data in temp)
+            //    {
+            //        textBox1.Text = textBox1.Text + "\r\n" + data;
+            //        textBox1.Select(textBox1.Text.Length, 0);
+            //        textBox1.ScrollToCaret();
+            //    }
+            //}
+
+        }
+
+        private void bwNotifications_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            if (mReader.GetCurrentMessages(out string[] Notifications) > 0)
+            {
+                string tl,reason;
+                ITagInfo[] tagInfos;
+                int start,end;
+
+                foreach (string notification in Notifications)
+                {
+                    SetText(textBox1, notification);
+                    start = notification.IndexOf(reasonStr) + reasonStr.Count();
+                    if (start > 0)
+                    {
+                        end = notification.IndexOf("\r\n",start);
+                        reason = notification.Substring(start,end - start);
+                        SetText(textBox1, reason);
+
+                        start = notification.IndexOf("Tag:", start);
+                        if (start > 0)
+                        {
+
+                            int ennd = notification.IndexOf("\r\n#End of Notification Message", start);
+
+                            tl = notification.Substring(start, ennd - start);
+
+                            if (mReader.ParseTagList(tl, out tagInfos))
+                            {
+                                //tagInfos.
+                                foreach (ITagInfo tag in tagInfos)
+                                {
+                                    SetText(textBox1, "Direction: " + tag.Direction + "Antenna: " + tag.Antenna + "TagID: " + tag.TagID + ",Read Count: " + tag.ReadCount + ",Last Seen:" + tag.LastSeenTime + "\r\n");
+                                    AddRow(dgvTracker, tag.TagID, reason.Contains("ADD") ? "Enter" : "Leave", DateTime.Now);
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+        private void bwNotifications_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bwNotifications.RunWorkerAsync();
+        }
+
+        private void bwConnect_DoWork(object sender, DoWorkEventArgs e)
+        {
+            mReader.InitOnCom(3);// ‘Initialize reader object on COM1
+            e.Result = mReader.Connect();
+
+        }
+
+        private void bwConnect_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string stemp;
+
+            stemp = e.Result.ToString();
+            textBox1.Text = textBox1.Text + "\r\n" + stemp + "\r\n";
+
+            if (stemp == "Connected")
+            {
+                mReader.NotifyMode = "ON";
+                mReader.AutoMode = "ON";
+                bwNotifications.RunWorkerAsync();
+            }
+            else
+            {
+                bwConnect.RunWorkerAsync();
+            }
+
+        }
+
+
+    }
+}
